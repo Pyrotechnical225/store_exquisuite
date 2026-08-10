30d78c7f2d37dc81aac9148069928e6193c11390
Pyrotechnical225 <anasrapgod69@gmail.com>
2026-08-10 23:49:39 +0100

    Add src/app/not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,2 @@
+import Link from "next/link";
+export default function NotFound() { return <section className="shell info-page"><p className="eyebrow dark">404</p><h1>We couldn’t find that page.</h1><Link className="button" href="/">Return home</Link></section>; }
